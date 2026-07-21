e74ca561f1d0619e086304f41f698a30625cd0db
Tao Zeyu <i@taozeyu.com>
2026-07-21 16:15:59 +0800

    refactor: route topology snake through module entry

--- packages/core/src/topology/core.ts
+++ packages/core/src/topology/core.ts
@@ -6,11 +6,11 @@ import type {
 } from "../document/index.js";
 import type { ReaderChunk, ReaderGraphDelta } from "../reader/index.js";
 import { groupSegments } from "./grouping.js";
-import { buildSnakeGraph } from "./snake/graph-builder.js";
 import {
+  buildSnakeGraph,
   detectSnakesInComponent,
   splitConnectedComponents,
-} from "./snake/detector.js";
+} from "./snake/index.js";
 import {
   computeChunkWeights,
   computeReadingEdgeWeights,
